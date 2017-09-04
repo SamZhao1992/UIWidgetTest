e7e315ac10ae3cb11f054f4be12b8f8b296b7e62
SamZhao1992 <samsamzhao@126.com>
2017-09-04 17:02:48 +0800

    Initial commit

---
+++ app/src/main/java/com/example/samzhao/uiwidgettest/FruitAdapter.java
@@ -0,0 +1,39 @@
+package com.example.samzhao.uiwidgettest;
+
+import android.content.Context;
+import android.support.annotation.LayoutRes;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+/**
+ * Created by SamZhao on 2017/9/4.
+ */
+
+public class FruitAdapter extends ArrayAdapter<Fruit> {
+    private int resourceId;
+
+    public FruitAdapter(@NonNull Context context, @LayoutRes int resource, @NonNull List<Fruit> objects) {
+        super(context, resource, objects);
+        resourceId = resource;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        Fruit fruit = getItem(position);
+        View view = LayoutInflater.from(getContext()).inflate(resourceId, parent, false);
+        ImageView imageView = (ImageView) view.findViewById(R.id.fruit_image);
+        TextView textView = (TextView) view.findViewById(R.id.fruit_name);
+        imageView.setImageResource(fruit.getImageId());
+        textView.setText(fruit.getName());
+        return view;
+    }
+}
---
+++ app/src/main/java/com/example/samzhao/uiwidgettest/MainActivity.java
@@ -0,0 +1,124 @@
+package com.example.samzhao.uiwidgettest;
+
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText editText;
+    private ImageView imageView;
+    private ProgressBar progressBar;
+
+    private String[] data= {"aaa", "bbb", "ccc", "ddd", "eee", "fff", "ggg", "hhh" , "iii", "jjj", "kkk",
+    "lll", "mmm", "nnn"};
+
+    private List<Fruit> fruitList = new ArrayList<Fruit>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null)
+            actionBar.hide();
+        Button button1 = (Button) findViewById(R.id.button1);
+        Button button2 = (Button) findViewById(R.id.button2);
+        editText = (EditText) findViewById(R.id.edit_text);
+        imageView = (ImageView) findViewById(R.id.img_view);
+        progressBar = (ProgressBar) findViewById(R.id.process_bar);
+        button1.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                String data = editText.getText().toString();
+                Toast.makeText(MainActivity.this, data, Toast.LENGTH_SHORT).show();
+                imageView.setImageResource(R.drawable.img_2);
+                int progress = progressBar.getProgress();
+                progress = 10 + progress;
+                progressBar.setProgress(progress);
+            }
+
+        });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder dialog = new AlertDialog.Builder(MainActivity.this);
+                dialog.setTitle("This is a dialog");
+                dialog.setMessage("Something is important!");
+                dialog.setCancelable(false);
+                dialog.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+                dialog.setNegativeButton("Cancle", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        ProgressDialog progressDialog = new ProgressDialog(MainActivity.this);
+                        progressDialog.setTitle("You click cancle!");
+                        progressDialog.setMessage("Cancling...");
+                        progressDialog.setCancelable(true);
+                        progressDialog.show();
+                    }
+                });
+                dialog.show();
+            }
+
+        });
+
+//        ArrayAdapter<String> adapter = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_list_item_1, data);
+//        ListView listView = (ListView) findViewById(R.id.list_view);
+//        listView.setAdapter(adapter);
+        
+        initFruits();
+
+        FruitAdapter fruitAdapter = new FruitAdapter(MainActivity.this, R.layout.fruit_item, fruitList);
+        ListView listView1 = (ListView) findViewById(R.id.list_fruits);
+        listView1.setAdapter(fruitAdapter);
+
+    }
+
+    private void initFruits() {
+        for (int i = 0; i < 1; i++) {
+            Fruit apple = new Fruit("Apple", R.drawable.apple_pic);
+            fruitList.add(apple);
+            Fruit banana = new Fruit("Banana", R.drawable.banana_pic);
+            fruitList.add(banana);
+            Fruit orange = new Fruit("Orange", R.drawable.orange_pic);
+            fruitList.add(orange);
+            Fruit watermelon = new Fruit("Watermelon", R.drawable.watermelon_pic);
+            fruitList.add(watermelon);
+            Fruit pear = new Fruit("Pear", R.drawable.pear_pic);
+            fruitList.add(pear);
+            Fruit grape = new Fruit("Grape", R.drawable.grape_pic);
+            fruitList.add(grape);
+            Fruit pineapple = new Fruit("Pineapple", R.drawable.pineapple_pic);
+            fruitList.add(pineapple);
+            Fruit strawberry = new Fruit("Strawberry", R.drawable.strawberry_pic);
+            fruitList.add(strawberry);
+            Fruit cherry = new Fruit("Cherry", R.drawable.cherry_pic);
+            fruitList.add(cherry);
+            Fruit mango = new Fruit("Mango", R.drawable.mango_pic);
+            fruitList.add(mango);
+        }
+    }
+
+}
